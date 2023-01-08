0bc9a9fcf1040fb0c89118c72ae65b4ee81f7883
L1so <pascal@japati.net>
2023-01-08 11:14:07 +0700

    Testing final

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 Kirim pesan WhatsApp langsung dari terminal, membutuhkan API dari WA Gateway.
 
+## Instalasi
+Melalui `git clone`.
+
+	git clone https://github.com/L1so/SendWA.git
+	cd SendWA
+	./SendWA --install
+
+Melalui `wget`.
+	
+	wget -O SendWA.zip https://github.com/L1so/SendWA/archive/refs/heads/main.zip
+	unzip SendWA.zip && cd SendWA-main
+	./SendWA --install
+Melalui `curl`.
+	
+	curl -o SendWA.zip https://github.com/L1so/SendWA/archive/refs/heads/main.zip
+	unzip SendWA.zip && cd SendWA-main
+	./SendWA --install
+Masukkan API Key dan URL pada proses instalasi.
 ## Program Prasyarat
 - ImageMagick &rarr; untuk kirim gambar.
 - `curl` atau `wget` &rarr; untuk melakukan api call.
@@ -21,4 +39,4 @@ Bagi provider yang mau diintegrasikan layanannya ke skrip ini, bisa kirim email 
 - API Key.
 - Nomor WhatsApp yang sudah disambungkan ke WA Gateway.
 
-Saya akan menanggapi formulir anda dalam waktu kurang lebih ~3 hari kerja jika tidak sibuk— kemudian ketika sudah terimplementasi, saya akan reply email anda dengan info lebih lanjut.
+Saya akan menanggapi formulir anda dalam waktu kurang lebih ~3 hari kerja jika tidak sibuk.
--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 # Update existing SendWA installation
 PROJECT_NAME="SendWA"; mp="$HOME/.sendwa"; envfile="$mp/file.env"
 [ -f "$envfile" ] && . "$envfile"
+_info() {
+  printf "%s\n" "$(date +"[%d %b %Y %l:%M:%S %p %Z]") $*"
+}
 _grab() {
   _link="$1"
   if command -v curl >/dev/null 2>&1; then
@@ -15,7 +18,7 @@ _grab() {
       *SendWA) curl -kLs "$_link" -o "$mp/sendwa.sh";;
     esac
   else
-    printf "%s\n" "Please install wget or curl"
+    _info "%s\n" "Please install wget or curl"
     exit 1
   fi
 }
@@ -23,13 +26,14 @@ _grab() {
 if [ -n "$GIT_SHA" ]; then
   REMOTE_SHA=$(_grab "https://raw.githubusercontent.com/L1so/SendWA/main/file.env" | grep "GIT_SHA" | cut -d '=' -f2)
   if [ "$REMOTE_SHA" != "$GIT_SHA" ]; then
-    printf "%s\n" "Downloading new version"
+    _info "%s\n" "Downloading new version"
     _grab "https://raw.githubusercontent.com/L1so/SendWA/main/SendWA" && chmod u+x "$mp/sendwa.sh"
-    printf "%s\n" "Success !"
+    sed "s/GIT_SHA=.*/GIT_SHA=${REMOTE_SHA}/" "${mp}/file.env" > "$mp/file.env.temp" && mv "$mp/file.env.temp" "$mp/file.env"
+    _info "Success !"
   else
-    printf "%s\n" "Already uptodate !"
+    _info "Already uptodate !"
   fi
 else
-  printf "%s\n" "Latest Git Hash not detected"
+  _info "%s\n" "Latest Git Hash not detected"
   exit 1
 fi
