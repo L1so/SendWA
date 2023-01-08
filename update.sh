8948f4c8870567d37b619f6b11ae3dc873e37e67
L1so <pascal@japati.net>
2023-01-08 10:00:53 +0700

    Modify updater

--- update.sh
+++ update.sh
@@ -6,13 +6,13 @@ _grab() {
   _link="$1"
   if command -v curl >/dev/null 2>&1; then
     case "$_link" in
-      api.github.com*) wget -qO- "$_link";;
-      raw.githubusercontent.com*) wget -q -O "$mp/sendwa.sh" "$_link";;
+      *file.env) wget -qO- "$_link";;
+      *SendWA) wget -q -O "$mp/sendwa.sh" "$_link";;
     esac
   elif command -v wget >/dev/null 2>&1; then
     case "$_link" in
-      api.github.com*) curl -kLs "$_link";;
-      raw.githubusercontent.com*) curl -kLs "$_link" -o "$mp/sendwa.sh";;
+      *file.env) curl -kLs "$_link";;
+      *SendWA) curl -kLs "$_link" -o "$mp/sendwa.sh";;
     esac
   else
     printf "%s\n" "Please install wget or curl"
@@ -21,7 +21,7 @@ _grab() {
 }
 # Check if sha match
 if [ -n "$GIT_SHA" ]; then
-  REMOTE_SHA=$(_grab "https://api.github.com/repos/L1so/${PROJECT_NAME}/git/refs/heads/main" | tr -d "\r\n" | tr '{},' '\n\n\n' | grep '"sha":' | cut -d '"' -f 4)
+  REMOTE_SHA=$(_grab "https://raw.githubusercontent.com/L1so/SendWA/main/file.env" | grep "GIT_SHA" | cut -d '=' -f2)
   if [ "$REMOTE_SHA" != "$GIT_SHA" ]; then
     printf "%s\n" "Downloading new version"
     _grab "https://raw.githubusercontent.com/L1so/SendWA/main/SendWA" && chmod u+x "$mp/sendwa.sh"
