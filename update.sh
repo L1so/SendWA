ff479774ca30c6f85b65abd0901f945c86c3269d
L1so <pascal@japati.net>
2023-01-30 10:53:17 +0700

    Update local

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Update existing SendWA installation
-PROJECT_NAME="SendWA"; mp="$HOME/.sendwa"; envfile="$mp/file.env"
-[ -f "$envfile" ] && . "$envfile"
+PROJECT_NAME="SendWA"
+if [ -d "/opt/sendwa" ]; then
+  . /opt/sendwa/file.env
+elif [ -d "$HOME/.sendwa" ]; then
+  . $HOME/.sendwa/file.env
+fi
 _info() {
   printf "%s\n" "$(date +"[%d %b %Y %l:%M:%S %p %Z]") $*"
 }
@@ -10,12 +14,12 @@ _grab() {
   if command -v curl >/dev/null 2>&1; then
     case "$_link" in
       *file.env) wget -qO- "$_link";;
-      *SendWA) wget -q -O "$mp/sendwa.sh" "$_link";;
+      *SendWA) wget -q -O "$WA_INSTALL_DIR/sendwa.sh" "$_link";;
     esac
   elif command -v wget >/dev/null 2>&1; then
     case "$_link" in
       *file.env) curl -kLs "$_link";;
-      *SendWA) curl -kLs "$_link" -o "$mp/sendwa.sh";;
+      *SendWA) curl -kLs "$_link" -o "$WA_INSTALL_DIR/sendwa.sh";;
     esac
   else
     _info "Please install wget or curl"
@@ -27,8 +31,8 @@ if [ -n "$GIT_SHA" ]; then
   REMOTE_SHA=$(_grab "https://raw.githubusercontent.com/L1so/SendWA/main/file.env" | grep "GIT_SHA" | cut -d '=' -f2)
   if [ "$REMOTE_SHA" != "$GIT_SHA" ]; then
     _info "Downloading new version"
-    _grab "https://raw.githubusercontent.com/L1so/SendWA/main/SendWA" && chmod u+x "$mp/sendwa.sh"
-    sed "s/GIT_SHA=.*/GIT_SHA=${REMOTE_SHA}/" "${mp}/file.env" > "$mp/file.env.temp" && mv "$mp/file.env.temp" "$mp/file.env"
+    _grab "https://raw.githubusercontent.com/L1so/SendWA/main/SendWA" && chmod u+x "$WA_INSTALL_DIR/sendwa.sh"
+    sed "s/GIT_SHA=.*/GIT_SHA=${REMOTE_SHA}/" "${WA_INSTALL_DIR}/file.env" > "$WA_INSTALL_DIR/file.env.temp" && mv "$WA_INSTALL_DIR/file.env.temp" "$WA_INSTALL_DIR/file.env"
     _info "Success !"
   else
     _info "Already uptodate !"
